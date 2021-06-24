204786b8ea3435d8d52551928502fefeac7b2a21
galetedanilo <galetedanilo@gmail.com>
2021-06-23 23:21:28 -0300

    Change: Controller to RestController

--- backend/src/main/java/com/devsuperior/events/controllers/CityController.java
+++ backend/src/main/java/com/devsuperior/events/controllers/CityController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.events.dto.CityDTO;
 import com.devsuperior.events.services.CityService;
 
-@Controller
+@RestController
 @RequestMapping(value = "/cities")
 public class CityController {
 
